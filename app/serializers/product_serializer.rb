50bbb0e1d8c3244db677c22af4fc39e55b8769bd
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-04 18:54:20 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -5,7 +5,7 @@ def self.serialize(product)
   serialized_product += '"id": ' + product.id.to_s + ', '
   serialized_product += '"name": "' + product.name + '", '
   serialized_product += '"description": "' + product.description + '", '
-  serialized_product += '"inventory": ' + product.inventory.to_s + '", '
+  serialized_product += '"inventory": ' + product.inventory.to_s + ', '
   serialized_product += '"price": "' + product.price.to_s + '"'
   serialized_product += '}'
 end
