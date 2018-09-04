9b711c4e9f71ee51ff6fe90e11006e3648427d11
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-04 18:48:09 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -3,8 +3,8 @@ class ProductSerializer
 def self.serialize(product)
   serialized_product = '{'
   serialized_product += '"id": ' + product.id.to_s + ','
-  serialized_product += '"name": '+ product.name + ','
-  serialized_product += '"inventory": '+ "#{product.inventory}" + ','
+  serialized_product += '"name": '+ product.name + '",'
+  serialized_product += '"inventory": '+ "#{product.inventory}" + '",'
   serialized_product += '"description": '+ product.description + '}'
 end
 
